8f0129e94daf12a2de08a5d1ac224e731b144015
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-07 22:03:26 +0100

    Band Name Generator

---
+++ Day1/Band Name Generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grew up in?\n")
+
+pet = input("What's the name of a pet?\n")
+
+print("Possible band name " + city + " " + pet)
