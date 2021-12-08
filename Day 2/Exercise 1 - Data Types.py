b9986c1d6a4b00445bfd2063e00f5ef79e384ccb
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-08 09:58:04 +0100

    21

---
+++ Day 2/Exercise 1 - Data Types.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+# two_digit_number_int = int(two_digit_number)
+# print(two_digit_number_int % 10 + two_digit_number_int // 10)
+
+####################################
+#Write your code below this line 👇
