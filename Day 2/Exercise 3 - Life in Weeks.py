6518ebc416811f6ebabc10603cba12759bcef360
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-08 10:22:41 +0100

    25

---
+++ Day 2/Exercise 3 - Life in Weeks.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?")
+# 🚨 Don't change the code above 👆
+
+age_left = 90 - int(age);
+
+print("You have {0} days, {1} weeks, and {2} months left.".format(age_left * 365, age_left * 52, age_left * 12))
+
+# print(f"You have {age_left * 365} days, {age_left * 52} weeks, and {2} months left.")
+
+# Write your code below this line 👇
