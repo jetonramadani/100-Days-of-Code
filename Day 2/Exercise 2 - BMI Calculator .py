6c2688ba2f965e304ed694e9464a5d099b9c4a98
jeton ramadani <jeton.ramadani@students.finki.ukim.mk>
2021-12-08 10:03:57 +0100

    23

---
+++ Day 2/Exercise 2 - BMI Calculator .py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+height_f = float(height)
+weight_f = float(weight)
+
+print(int(weight_f / height_f ** 2))
+# Write your code below this line 👇
